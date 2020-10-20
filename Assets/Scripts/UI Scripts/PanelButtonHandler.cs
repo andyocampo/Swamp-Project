bb88784a08cf988fbd8d3e72107ae68c05917bdc
Andy <andyocampo@users.noreply.github.com>
2020-10-20 15:10:30 -0500

    Adds open panel animation, open panel script and button

---
+++ Assets/Scripts/UI Scripts/PanelButtonHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelButtonHandler : MonoBehaviour
+{
+    [SerializeField]
+    GameObject Panel;
+
+    public void OpenPanel() //opens and closes UI panel
+    {
+        Animator animator = Panel.GetComponent<Animator>();
+        bool isOpen = animator.GetBool("is Open");
+
+        animator.SetBool("is Open", !isOpen);
+    }
+}
