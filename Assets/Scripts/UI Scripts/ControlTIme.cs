7f0c8aa8b419b9260a22b4532d9784af4909b80e
Andy <51983062+andyocampo@users.noreply.github.com>
2020-10-21 23:59:24 -0500

    Adds time changing mechanic and button assests, changes frog movement script

---
+++ Assets/Scripts/UI Scripts/ControlTIme.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlTIme : MonoBehaviour
+{
+
+    public void PlayOnClick()
+    {
+        Time.timeScale = 1;
+    }
+
+    public void PauseOnClick()
+    {
+        Time.timeScale = 0;
+    }
+
+    public void SlowOnClick()
+    {
+        Time.timeScale = 0.5f;
+    }
+
+    public void FastOnClick()
+    {
+        Time.timeScale = 2;
+    }
+
+}
