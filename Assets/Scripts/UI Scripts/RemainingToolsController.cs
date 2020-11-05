9b2bd8b4bb2f855456e4fa06942a6cc758ac6112
Andy <andyocampo@users.noreply.github.com>
2020-11-05 06:29:45 -0600

    Adds remaining tools handler and controller that changes when the player uses a tool

---
+++ Assets/Scripts/UI Scripts/RemainingToolsController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class RemainingToolsController : MonoBehaviour
+{
+    private TMP_Text messageText;
+    void Start()
+    {
+        messageText = GetComponent<TMP_Text>();
+        messageText.text = $"# of Tools";
+    }
+
+    private void UpdateUI(string text)
+    {
+        messageText.text = text;
+    }
+
+    private void OnRemainingToolsTriggered(string text)
+    {
+        UpdateUI(text);
+    }
+
+    private void OnEnable()
+    {
+        if (name == "Tool 1 Remaining")
+        {
+            RemainingToolsHandler.RemainingTools1Triggered += OnRemainingToolsTriggered;
+        }
+        else if (name == "Tool 2 Remaining")
+        {
+            RemainingToolsHandler.RemainingTools2Triggered += OnRemainingToolsTriggered;
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (name == "Tool 1 Remaining")
+        {
+            RemainingToolsHandler.RemainingTools1Triggered -= OnRemainingToolsTriggered;
+        }
+        else if (name == "Tool 2 Remaining")
+        {
+            RemainingToolsHandler.RemainingTools2Triggered -= OnRemainingToolsTriggered;
+        }
+    }
+}
---
+++ Assets/Scripts/UI Scripts/RemainingToolsHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemainingToolsHandler : MonoBehaviour
+{
+    [SerializeField]GameObject playerEditor;
+    public static event Action<string> RemainingTools1Triggered;
+    public static event Action<string> RemainingTools2Triggered;
+    void Update()
+    {
+        PlayerLevelEditor playerLevelEditor = playerEditor.GetComponent<PlayerLevelEditor>();
+        string RemainingTool1Text;
+        string RemainingTool2Text;
+        if (transform.GetChild(0))
+        {
+            RemainingTool1Text = $"{playerLevelEditor.tilesRemaining[0]}x";
+            if (RemainingTools1Triggered != null)
+            {
+                RemainingTools1Triggered.Invoke(RemainingTool1Text);
+            }
+        }
+        if (transform.GetChild(1))
+        {
+            RemainingTool2Text = $"{playerLevelEditor.tilesRemaining[1]}x";
+            if (RemainingTools2Triggered != null)
+            {
+                RemainingTools2Triggered.Invoke(RemainingTool2Text);
+            }
+        }
+    }
+}
