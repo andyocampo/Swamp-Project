525d2dded8676985d4a320f74f3ee3a9a56d918c
Andy <andyocampo@users.noreply.github.com>
2020-11-12 05:43:59 -0600

    Fixes Win/Lose UI so that the start message does not interfere with buttons (DeactiveStartMessage script)

---
+++ Assets/DeactiveStartMessage.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DeactiveStartMessage : MonoBehaviour
+{
+    TMP_Text tmPro;
+
+    private void Start()
+    {
+        tmPro = GetComponent<TMP_Text>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(tmPro.color.a <= 0)
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+}
