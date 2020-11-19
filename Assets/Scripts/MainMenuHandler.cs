dba47c5bc26e2955d2840ee84265a5cf4aed3c66
Andy <andyocampo@users.noreply.github.com>
2020-11-19 14:34:15 -0600

    Adds Menu functionality, renames some stuff in the scene

--- Assets/Scripts/MainMenuHandler.cs
+++ Assets/Scripts/MainMenuHandler.cs
@@ -1,18 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void StartGame()
     {
-        
+        SceneManager.LoadScene("TestLevel");
     }
 
-    // Update is called once per frame
-    void Update()
+    //exits game
+    public void Exit()
     {
-        
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#elif UNITY_WEBPLAYER
+         Application.OpenURL(webplayerQuitURL);
+#else
+         Application.Quit();
+#endif
     }
 }
