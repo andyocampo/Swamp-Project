5ee609b9c93dfc9b6d71eab213e9c7ada4783c5f
Andy <andyocampo@users.noreply.github.com>
2020-12-10 07:54:56 -0600

    Adds Background Music

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    private static SoundManager instance;
+
+    public static SoundManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                Debug.LogError("Sound Manager does not exist!!!");
+            }
+
+            return instance;
+        }
+    }
+
+    public void PlayPlayerClick()
+    {
+        
+    }
+
+    public void PlayFrogJump()
+    {
+
+    }
+
+    public void PlayFrogGoal()
+    {
+
+    }
+}
